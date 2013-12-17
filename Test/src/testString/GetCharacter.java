420012f0c03994fd60fb6437e4e92b0b067016b1
Leikai Ma <leikai.ma@gmail.com>
2013-12-16 20:43:30 -0500

    leetcode wildcard

---
+++ Test/src/testString/GetCharacter.java
@@ -0,0 +1,9 @@
+package testString;
+// 如果是empty 不能得到character
+public class GetCharacter {
+	public static void main(String[] args) {
+		String string = "";
+//		System.out.println(string.charAt(0));
+		System.out.println(string.toCharArray().length);
+	}
+}
--- Test/src/testString/SubStringExceedLength.java
+++ Test/src/testString/SubStringExceedLength.java
@@ -5,8 +5,12 @@ public class SubStringExceedLength {
 //	说明substring 不能超过最大限制。
 	public static void main(String[] args) {
 		String s = "abc";
-		String subString = s.substring(-1, 2);
+//		String subString = s.substring(-1, 2);
 //		System.out.println(subString);
+//		System.out.println(subString);
+		
+		
+		String subString = s.substring(4);
 		System.out.println(subString);
 	}
 
