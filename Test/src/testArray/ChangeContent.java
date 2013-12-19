8ab967288e95251fbfd0ce397858012495924453
Leikai Ma <leikai.ma@gmail.com>
2013-12-19 02:36:04 -0500

    surrounded regions

--- Test/src/testArray/ChangeContent.java
+++ Test/src/testArray/ChangeContent.java
@@ -2,9 +2,10 @@ package testArray;
 
 public class ChangeContent {
 	private static void change(char[][] a) {
-		char[][] b = new char[9][9];
-		b = a.clone();
-		b[0][0] = '1';
+//		char[][] b = new char[9][9];
+//		b = a.clone();
+//		b[0][0] = '1';
+		a[0][0] = '1';
 	}
 
 	public static void main(String[] args) {
