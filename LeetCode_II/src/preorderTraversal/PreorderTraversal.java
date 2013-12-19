d5e50a21923f250adef768cd763da904b845537c
Leikai Ma <leikai.ma@gmail.com>
2013-12-18 21:48:28 -0500

    leetcode preorder traversal

---
+++ LeetCode_II/src/preorderTraversal/PreorderTraversal.java
@@ -0,0 +1,24 @@
+package preorderTraversal;
+// 要注意的就是如果是null 要返回的是一个空的arraylist 而不是null
+import java.util.ArrayList;
+
+public class PreorderTraversal {
+	public ArrayList<Integer> preorderTraversal(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if (root == null) {
+			return result;
+		}
+		preorderTraversal(root, result);
+		return result;
+	}
+
+	private static void preorderTraversal(TreeNode root,
+			ArrayList<Integer> result) {
+		if (root == null) {
+			return;
+		}
+		result.add(root.val);
+		preorderTraversal(root.left, result);
+		preorderTraversal(root.right, result);
+	}
+}
