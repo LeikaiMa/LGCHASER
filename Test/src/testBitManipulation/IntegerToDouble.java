5e4101d5dc486410f55e12c6d4e4b62686a0c12c
Leikai Ma <leikai.ma@gmail.com>
2013-12-31 17:43:49 -0500

    leetcode max points in a line

---
+++ Test/src/testBitManipulation/IntegerToDouble.java
@@ -0,0 +1,11 @@
+package testBitManipulation;
+// 乘以1.0 避免强制转换
+public class IntegerToDouble {
+	public static void main(String[] args) {
+		int a = 1;
+		int b =2;
+		int c =3;
+		int d =6;
+		System.out.println((b-a) * 1.0 /(c-d));
+	}
+}
