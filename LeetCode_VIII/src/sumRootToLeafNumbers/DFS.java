874ebdb8f914480136bb36129c8d518202a2dc84
Leikai Ma <leikai.ma@gmail.com>
2014-01-04 01:23:01 -0500

    leetcode sum root to leaves numbers

---
+++ LeetCode_VIII/src/sumRootToLeafNumbers/DFS.java
@@ -0,0 +1,51 @@
+package sumRootToLeafNumbers;
+
+import java.util.ArrayList;
+// 这个是简单的tree 的DFS的问题，因为是用int 所以不要加了之后再减去，因为每次都生成的是新的。
+// 从root 开始，每读一个就在原来基础上*10 + 现在这个值。
+// 然后全部存在一个arraylist 里面，最后统一相加，
+// 但是这个要注意的是区别于之前的Tree 的dfs, 这个不能是在node 为null 的时候添加一个，这样的话就在在叶子上重复写了两遍。
+// 所以应该是在看左边和右边都没有的情况下就添加返回，然后如果有一个有的话就去那一个。两个有的话就两个都去。
+// 小细节在没有DEBUG工具的情况下要小心。
+public class DFS {
+	public static int sumNumbers(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+
+		ArrayList<Integer> results = new ArrayList<Integer>();
+		int number = 0;
+		sumNumbersHelper(root, results, number);
+		int sum = 0;
+		for (int r : results) {
+			sum += r;
+		}
+		return sum;
+	}
+
+	private static void sumNumbersHelper(TreeNode root,
+			ArrayList<Integer> results, int number) {
+
+		number = number * 10 + root.val;
+		if (root.left == null && root.right == null) {
+			results.add(number);
+			return;
+		}
+		if (root.left != null) {
+			sumNumbersHelper(root.left, results, number);
+		}
+		if (root.right != null) {
+			sumNumbersHelper(root.right, results, number);
+		}
+	}
+
+	public static void main(String[] args) {
+		TreeNode t1 = new TreeNode(1);
+		TreeNode t2 = new TreeNode(2);
+		TreeNode t3 = new TreeNode(3);
+		t1.left = t2;
+		t1.right = t3;
+
+		System.out.println(sumNumbers(t1));
+	}
+}
